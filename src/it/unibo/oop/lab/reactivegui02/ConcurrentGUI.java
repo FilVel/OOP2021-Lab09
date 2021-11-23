bc185b89c872d3554ad0fe4c9021020a88eeb436
Filippo Velli <filippo.velli@studio.unibo.it>
2021-11-23 16:33:50 +0100

    Completed excercise reactivegui02

---
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -0,0 +1,112 @@
+package it.unibo.oop.lab.reactivegui02;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public final class ConcurrentGUI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+	private final JLabel display = new JLabel();
+	private final JButton up = new JButton("up");
+	private final JButton down = new JButton("down");
+	private final JButton stop = new JButton("stop");
+	
+	public ConcurrentGUI() {
+		super();
+		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setSize((int)(screenSize.getWidth()*WIDTH_PERC), (int)(screenSize.getHeight()*HEIGHT_PERC));
+		final JPanel panel = new JPanel();
+		panel.add(display);
+		panel.add(up);
+		panel.add(down);
+		panel.add(stop);
+		this.setContentPane(panel);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		final Agent agent = new Agent();
+        new Thread(agent).start();
+        
+        up.addActionListener(new ActionListener() {
+
+			@Override
+            public void actionPerformed(ActionEvent e) {
+                agent.upDirection();
+			}
+        });
+        
+        down.addActionListener(new ActionListener() {
+
+            @Override
+			public void actionPerformed(ActionEvent e) {
+				if(!agent.subtract) {
+					agent.downDirection();
+				}		
+			}
+        });
+        
+        stop.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                agent.stopCounting();
+                down.setEnabled(false);
+                up.setEnabled(false);
+                stop.setEnabled(false);
+            }
+        });
+	}
+	
+	private class Agent implements Runnable{
+
+		private volatile boolean stop = false;
+		private volatile boolean subtract = false;
+		private volatile int counter = 0;
+		@Override
+		public void run() {
+			try {
+				while (!stop) {
+                    SwingUtilities.invokeAndWait(new Runnable() {
+                        @Override
+                        public void run() {
+                            ConcurrentGUI.this.display.setText(Integer.toString(Agent.this.counter));
+                        }
+                    });
+                    Thread.sleep(100);
+                    if(!subtract) {
+                    	this.counter++;
+                    } else {
+                    	this.counter--;
+                    }
+				}
+            } catch (InvocationTargetException | InterruptedException ex) {
+            	ex.printStackTrace();
+            }
+		}
+		
+		public void upDirection() {
+            this.subtract=false;
+		}
+		
+		public void downDirection() {
+            this.subtract=true;
+		}
+
+
+		public void stopCounting() {
+			this.stop=true;
+		}
+		
+	}
+}
