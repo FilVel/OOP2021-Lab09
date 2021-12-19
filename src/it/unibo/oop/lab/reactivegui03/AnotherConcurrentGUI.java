ad4deb6b0c861eb6fd8a99a86172d37fc1e2f138
Filippo Velli <filippo.velli@studio.unibo.it>
2021-12-19 21:31:24 +0100

    Quick fixes

--- src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
@@ -74,6 +74,7 @@ public final class AnotherConcurrentGUI extends JFrame {
                 stop.setEnabled(false);
             }
         });
+
         new Thread(new Runnable() {
             @Override
             public void run() {
