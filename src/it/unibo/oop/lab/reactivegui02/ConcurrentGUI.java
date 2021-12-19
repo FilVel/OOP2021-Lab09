0b624247666f02ae4a5a3899c400c69a342f5354
Filippo Velli <filippo.velli@studio.unibo.it>
2021-12-19 20:55:33 +0100

    Small fixes inside of the Agent class

--- src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -12,52 +12,58 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+/**
+ *
+ */
 public final class ConcurrentGUI extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private static final double WIDTH_PERC = 0.2;
+    private static final long serialVersionUID = 1L;
+    private static final double WIDTH_PERC = 0.2;
     private static final double HEIGHT_PERC = 0.1;
-	private final JLabel display = new JLabel();
-	private final JButton up = new JButton("up");
-	private final JButton down = new JButton("down");
-	private final JButton stop = new JButton("stop");
-	
-	public ConcurrentGUI() {
-		super();
-		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setSize((int)(screenSize.getWidth()*WIDTH_PERC), (int)(screenSize.getHeight()*HEIGHT_PERC));
-		final JPanel panel = new JPanel();
-		panel.add(display);
-		panel.add(up);
-		panel.add(down);
-		panel.add(stop);
-		this.setContentPane(panel);
-		this.setVisible(true);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		final Agent agent = new Agent();
+    private final JLabel display = new JLabel();
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+
+    /**
+     *  Builds a GUI according to the specifications of the exercise.
+     */
+    public ConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.setContentPane(panel);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        final Agent agent = new Agent();
         new Thread(agent).start();
-        
+
         up.addActionListener(new ActionListener() {
 
-			@Override
-            public void actionPerformed(ActionEvent e) {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
                 agent.upDirection();
-			}
+            }
         });
-        
+
         down.addActionListener(new ActionListener() {
 
             @Override
-			public void actionPerformed(ActionEvent e) {
-				if(!agent.subtract) {
-					agent.downDirection();
-				}		
-			}
+            public void actionPerformed(final ActionEvent e) {
+                if (!agent.subtract) {
+                    agent.downDirection();
+                }
+            }
         });
-        
+
         stop.addActionListener(new ActionListener() {
-            
+
             @Override
             public void actionPerformed(final ActionEvent e) {
                 agent.stopCounting();
@@ -66,17 +72,18 @@ public final class ConcurrentGUI extends JFrame {
                 stop.setEnabled(false);
             }
         });
-	}
-	
-	private class Agent implements Runnable{
+    }
 
-		private volatile boolean stop = false;
-		private volatile boolean subtract = false;
-		private volatile int counter = 0;
-		@Override
-		public void run() {
-			try {
-				while (!stop) {
+    private class Agent implements Runnable {
+
+        private volatile boolean stop;
+        private volatile boolean subtract;
+        private volatile int counter;
+
+        @Override
+        public void run() {
+            try {
+                while (!stop) {
                     SwingUtilities.invokeAndWait(new Runnable() {
                         @Override
                         public void run() {
@@ -84,29 +91,28 @@ public final class ConcurrentGUI extends JFrame {
                         }
                     });
                     Thread.sleep(100);
-                    if(!subtract) {
-                    	this.counter++;
+                    if (!subtract) {
+                        this.counter++;
                     } else {
-                    	this.counter--;
+                        this.counter--;
                     }
-				}
+                }
             } catch (InvocationTargetException | InterruptedException ex) {
-            	ex.printStackTrace();
+                ex.printStackTrace();
             }
-		}
-		
-		public void upDirection() {
-            this.subtract=false;
-		}
-		
-		public void downDirection() {
-            this.subtract=true;
-		}
+        }
 
+        public void upDirection() {
+            this.subtract = false;
+        }
 
-		public void stopCounting() {
-			this.stop=true;
-		}
-		
-	}
+        public void downDirection() {
+            this.subtract = true;
+        }
+
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+    }
 }
